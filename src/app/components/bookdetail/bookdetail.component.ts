6b7e2322f5c7403c39c39e7382884262f80ff76c
Moli2112 <mattia.molinari.stud@ispascalcomandini.it>
2025-05-27 10:17:47 +0200

    Sprint 1

---
+++ src/app/components/bookdetail/bookdetail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-bookdetail',
+  imports: [],
+  templateUrl: './bookdetail.component.html',
+  styleUrl: './bookdetail.component.css'
+})
+export class BookdetailComponent {
+
+}
